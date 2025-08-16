b8cb806e2160ed3e73345952a458e09394ebf34a
Georgi Nersisyan <chelchik90@gmail.com>
2025-08-16 18:54:00 +0400

    add-register-styles

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,18 +1,43 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./nav";
 import { menuItems } from "./menu-items";
 import ProfileIcon from "./profileIcon";
 import CreatePostBtn from "./create-post-btn";
-import { IPost, postItems } from "./post-items";
 import LoginBtn from "./login-btn";
 import SigninBtn from "./sign-btn";
+import { useAuth } from "../contexts/AuthContext";
 
-export default function Header() { 
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+export interface isLoggedInProps {
+  setIsLoggedIn: (val: boolean) => void;
+}
+
+export default function Header() {
+  const {isLoggedIn, setIsLoggedIn} = useAuth();
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:5000/me", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (res.ok) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (err) {
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkLogin();
+  }, []);
 
   return (
     <header className="flex items-center justify-between px-5 py-0.5 bg-header-bg text-white sticky top-0 z-100">
@@ -27,18 +52,17 @@ export default function Header() {
       </Link>
       <Nav MenuItems={menuItems} />
 
-      {
-        isLoggedIn ?
+      {isLoggedIn ? (
         <div className="flex items-center gap-8">
-        <CreatePostBtn />
-        <ProfileIcon />
+          <CreatePostBtn />
+          <ProfileIcon />
         </div>
-        :
-        <div className="flex items-center gap-4"> 
+      ) : (
+        <div className="flex items-center gap-4">
           <LoginBtn />
           <SigninBtn />
         </div>
-      }
+      )}
     </header>
   );
 }
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,42 +1,92 @@
-import React from 'react'
+"use client";
 
-export default function Login() {
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setMessage(data.message);
+        setIsError(false);
+        setIsLoggedIn(true);
+        router.push("/profile");
+      } else {
+        setMessage(data.error || "Ошибка авторизации");
+        setIsError(true);
+      }
+    } catch (err) {
+      setMessage("Сервер недоступен");
+      setIsError(true);
+    }
+    setUsername("");
+    setPassword("");
+  };
+
   return (
-    <div className='w-full p-6 flex flex-col justify-center items-center'>
-
-      <form action="/profile" className='w-xl flex flex-col gap-7 p-2.5'>
-        <h3 className='text-5xl text-start'>Log in</h3>
-        
-        <div className="w-full flex flex-col gap-2">
-          <input
-            type="text"
-            placeholder="Enter Your First Name*"
-            className={"w-full p-2.5 rounded-2xl border-2 border-primary-border text-xl"}
-          />
-        </div>
-
-        <div className="w-full flex flex-col gap-2">
-          <input
-            type="email"
-            placeholder="Enter Your Email*"
-            className={"w-full p-2.5 rounded-2xl border-2 border-primary-border text-xl"}
-          />
-        </div>
-
-        <div className="w-full flex flex-col gap-2">
-          <input
-            type="password"
-            placeholder="Enter Your Password*"
-            className={"w-full p-2.5 rounded-2xl border-2 border-primary-border text-xl"}
-          />
-        </div>
+    <div className="p-4 flex flex-col gap-3 justify-center items-center">
+      <form onSubmit={handleSubmit} className="w-xl flex flex-col gap-7 p-2.5">
+        <h1 className="text-5xl font-bold">Log in</h1>
 
         <input
-          type="submit"
-          value="Create"
-          className="w-full p-2 bg-submit border-submit border-2 border-solid transition-all rounded-2xl cursor-pointer hover:bg-transparent hover:text-submit"
+          type="text"
+          placeholder="Username"
+          className={
+            "w-full p-2.5 rounded-2xl border-2 text-xl" +
+            `${isError ? " border-error" : " border-primary-border"} ${
+              isError ? " text-error" : " text-foreground"
+            }`
+          }
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
+        <input
+          type="password"
+          placeholder="Password"
+          className={
+            "w-full p-2.5 rounded-2xl border-2 text-xl" +
+            `${isError ? " border-error" : " border-primary-border"} ${
+              isError ? " text-error" : " text-foreground"
+            }`
+          }
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="w-full p-2 bg-login border-login border-2 border-solid transition-all rounded-2xl cursor-pointer hover:bg-transparent hover:text-login hover:text-xl"
+        >
+          Войти
+        </button>
       </form>
+      {message && (
+        <p
+          className={
+            "mt-2 text-lg " + `${isError ? "text-error " : "text-foreground"}`
+          }
+        >
+          {message}
+        </p>
+      )}
     </div>
-  )
+  );
 }
